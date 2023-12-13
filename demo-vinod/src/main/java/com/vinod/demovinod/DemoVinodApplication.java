d8c093ebe50368e00139c64314e71a66090d0aee
vinodkanna1 <vinodkanna789@gmail.com>
2023-12-13 21:09:47 +0530

    Initial commit

---
+++ demo-vinod/src/main/java/com/vinod/demovinod/DemoVinodApplication.java
@@ -0,0 +1,13 @@
+package com.vinod.demovinod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoVinodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoVinodApplication.class, args);
+	}
+
+}
---
+++ demo-vinod/src/test/java/com/vinod/demovinod/DemoVinodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinod.demovinod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoVinodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
